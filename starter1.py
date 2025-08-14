bd37947dc62358437b06de4eadb9314944cd21c3
MasonT23127 <masont.23127@obhs.school.nz>
2025-08-15 10:20:57 +1200

    Update starter1.py

--- starter1.py
+++ starter1.py
@@ -1,2 +1,11 @@
+'''
+    author: Mason Twemlow
+    date: 15/08/2025
+    version: 1
+    description: This is going to be battle code
+'''
+
+
+
 while True: # Making a loop that goes forever
     letter = str(input('Enter a letter: ')) # Asking the user for a letter
