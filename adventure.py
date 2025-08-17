012408f576a80119e5a43494ed2e1bf9591d6a7b
MasonT23127 <masont.23127@obhs.school.nz>
2025-08-18 11:29:12 +1200

    Update adventure.py

--- adventure.py
+++ adventure.py
@@ -7,7 +7,7 @@
 
 #-------------libraries------------------
 
-import random 
+import random # Get a random integer, shuffle, ...
 
 
 #-------------functions-------------------
@@ -19,15 +19,36 @@ en_dff = 13 # enemies defense
 
 #------------main routine-----------------
 while(hp > 0 and en_hp > 0): # checking if both are alive 
-    attack = input('''
-        Enter your attack: 
-        [s]word 
-        [a]xe 
-        [h]eal
-    ''') # Asking the user for a letter
-    if(attack =='s'):
+    
+    # This loops checks for user input validation. They can only select s, a, or h.
+    while(True): 
+        try: 
+            attack = input('''
+                Enter your attack: 
+                [s]word 
+                [a]xe 
+                [h]eal
+            ''') # Asking the user for attack
+            if(attack[0].lower() == 's' or attack[0].lower() == 'a' or attack[0].lower() == 'h'): # Validate the input. 
+                break 
+            else: 
+                print('Invalid, enter "s", "a" or "h" ')
+        except: 
+            print('Invalid, try again.')
+        
+
+    # Normalize input to lowercase and use only the first character
+    action = attack[0].lower()
+
+    # Checking the user input 
+    if(action == 's'): # When the user enters s for sword attack
+        rand = random.randint(1,20)
+        if(rand > en_dff):
+            damage = random.randint(1,20)
+            print(f'You attack with sword doing {damage} damage')
+            en_hp -= damage 
+       
+    elif(action == 'a'): 
         pass 
-    elif(attack == 'a'):
-        pass 
-    elif(attack == 'h'):
+    elif(action == 'h'):
         pass 
